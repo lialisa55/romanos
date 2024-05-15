2d69437790d7f96b412522ad4c5b0fb9d2f854d1
Nathalia Monalisa <119463033+lialisa55@users.noreply.github.com>
2024-05-14 21:06:08 -0300

    programa inicial

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for(int j = 0; j < n; j++){
+        int a, m = 0, d = 0, c = 0, l = 0, x = 0, v = 0, i = 0;
+        scanf("%d", &a);
+        while (a >= 1000){
+            m++;
+            a -= 1000;
+        }
+    }
+    return 0;
+}
