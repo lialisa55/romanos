aba3221ead0e680bb6d0b3ec42e9079bbb13d663
lialisa55 <nathaliamonalisa150@gmail.com>
2024-05-15 00:42:06 -0300

    programa funcional. existe sala para melhora.

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void romanar(int num);
+
 int main()
 {
     int n;
     scanf("%d", &n);
 
     for(int j = 0; j < n; j++){
-        int a, m = 0, d = 0, c = 0, l = 0, x = 0, v = 0, i = 0;
-        scanf("%d", &a);
-        while (a >= 1000){
-            m++;
-            a -= 1000;
-        }
+        int a;
+	scanf("%d", &a);
+	romanar(a);
+	printf("\n");
     }
     return 0;
+}
+
+void romanar(int num){
+	int decimais[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	char *romanos[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+	for (int i = 0; i < 13; i++){
+		while (num >= decimais[i]){
+			printf("%s", romanos[i]);
+			num -= decimais[i];
+		}
+	}
 }
